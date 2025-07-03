b20f7807b6c17d320439d1051d4a49b3be71e698
rtuygs <aymennasri96735@gmail.com>
2025-07-03 08:58:52 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// تهيئة Firebase باستخدام الإعدادات الخاصة بك
+const firebaseConfig = {
+    apiKey: "AIzaSyDGviVk2XLBnAKYcyfT3Vvh--RPbTiXjqw",
+    authDomain: "jjjjjj-ae63a.firebaseapp.com",
+    projectId: "jjjjjj-ae63a",
+    storageBucket: "jjjjjj-ae63a.firebasestorage.app",
+    messagingSenderId: "794999406759",
+    appId: "1:794999406759:web:3e1846f24e2672c6131b82",
+    measurementId: "G-TQ1HQQ6CH8"
+};
+
+// تهيئة التطبيق
+firebase.initializeApp(firebaseConfig);
